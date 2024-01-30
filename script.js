33b70b48ff71b611fa79c5f386d604630c96b9fb
Aditya Gundla <151802795+Aditya178510@users.noreply.github.com>
2024-01-30 09:01:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+
+
+
+
+window.addEventListener('load', function() {
+    const mediaQuery = window.matchMedia('(max-width: 470px)');
+  
+    function handleMediaQueryChange(event) {
+      if (event.matches) {
+        // Media query matches (max-width: 470px)
+        gsap.from('.z', {
+          x:-1100,
+          duration:1,
+          delay:.7,
+          ease: "power2.inOut",
+          stagger:.8
+        });
+      } else {
+        // Media query doesn't match (max-width: 470px)
+        gsap.from('.z', {
+            x:50,
+            y:-50,
+            duration:1,
+            delay:.7,
+            ease: "power2.inOut",
+            stagger:.8
+        });
+      }
+    }
+  
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+  });
+//For Boxes)
+  window.addEventListener('load', function() {
+    const mediaQuery = window.matchMedia('(max-width: 470px)');
+  
+    function handleMediaQueryChange(event) {
+      if (event.matches) {
+        // Media query matches (max-width: 470px)
+        gsap.from('.game', {
+            x:-1200,
+            opacity:0,
+            duration:.1,
+            delay:.5,
+            ease: "power4.inOut", 
+            scrollTrigger:{
+                trigger:".game",
+                scroller:"body",
+                start:"top 10%",
+                markers:true,
+                end:"bottom bottom"
+            }
+        
+        });
+      } else {
+        // Media query doesn't match (max-width: 470px)
+        gsap.from('.game', {
+            x:-1200,
+            opacity:0,
+            duration:.1,
+            delay:.5,
+            ease: "power4.inOut", 
+            scrollTrigger:{
+                trigger:".game",
+                scroller:"body",
+                start:"top 10%",
+                markers:true,
+                end:"bottom bottom"
+            }
+        
+        });
+      }
+    }
+  
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+  });
+
